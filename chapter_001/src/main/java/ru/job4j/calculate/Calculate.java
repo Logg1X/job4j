2ba3d1dced6bdf4b0200560c70e5e35f8bdd8199
Pavel Toporov <per4mancerror@gmail.com>
2018-06-03 14:22:36 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+* Class Calculate, для вычисления арифметических операций.
+* @author Pavel Toporov.
+* @since 03.06.2018.
+* @version 0.1.
+*/
 public class Calculate{
+	/**
+	* main - Точка входа в программу.
+	* Вывод строки в консоль.
+	* @param args - args.
+	*/
 	 public static void main(String[] args) {
         System.out.println("Hello World");
     }	
