bb47e4b248bfceed63330f78893761a89cca8730
Pavel Toporov <per4mancerror@gmail.com>
2018-06-04 20:14:40 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,27 +1,30 @@
 package ru.job4j.calculate;
 
 /**
-* Class Calculate, для вычисления арифметических операций.
-* @author Pavel Toporov.
-* @since 03.06.2018.
-* @version 0.1.
-*/
-public class Calculate{
-	/**
+ * Class Calculate, для вычисления арифметических операций.
+ *
+ * @author Pavel Toporov.
+ * @version 0.1.
+ * @since 03.06.2018.
+ */
+public class Calculate {
+    /**
      * Method echo.
+     *
      * @param name Your name.
      * @return Echo plus your name.
      */
     public String echo(String name) {
         return "Echo, echo, echo : " + name;
-    }	
-	
-	/**
-	* main - Точка входа в программу.
-	* Вывод строки в консоль.
-	* @param args - args.
-	*/
-	 public static void main(String[] args) {
+    }
+
+    /**
+     * main - Точка входа в программу.
+     * Вывод строки в консоль.
+     *
+     * @param args - args.
+     */
+    public static void main(String[] args) {
         System.out.println("Hello World");
-    }	
+    }
 }
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -1,25 +1,28 @@
 package ru.job4j.calculate;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test
-* @author Pavel Toporov (per4mancerror@gmail.com).
-* @version $id$
-* @since 0.1
-*/
+ * Test
+ *
+ * @author Pavel Toporov (per4mancerror@gmail.com).
+ * @version $id$
+ * @since 0.1
+ */
 
-public class CalculateTest{
-	/**
-	* Test echo.
-	*/ @Test
-	public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Toporov Pavel";
-    String expect = "Echo, echo, echo : Toporov Pavel";
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-	}	
+public class CalculateTest {
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Toporov Pavel";
+        String expect = "Echo, echo, echo : Toporov Pavel";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 }
