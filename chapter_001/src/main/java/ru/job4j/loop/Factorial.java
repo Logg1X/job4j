6a29be4c11798d937ff451de9457622bfb9b6e33
Toporov Pavel <per4mancerror@gmail.com>
2018-06-09 12:45:12 +0300

    5.2. Создать программу вычисляющую факториал. [#193]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+/**
+ * @author Toporov Pavel (mailto:per4mancerror@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Factorial {
+    /**
+     * Вычисление факториала.
+     *
+     * @param n факториал числа n.
+     * @return Факториал.
+     */
+
+
+    public int calc(int n) {
+        int calc = 1;
+        for (int i = 1; i <= n; i++) {
+            calc *= i;
+        }
+        return calc;
+    }
+
+
+}
