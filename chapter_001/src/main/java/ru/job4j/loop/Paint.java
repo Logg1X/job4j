41d134ac4b3794989bca03444d55a0705a3d14fe
Toporov Pavel <per4mancerror@gmail.com>
2018-06-09 17:22:49 +0300

    5.4. Построить пирамиду в псевдографике. [#4412]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,31 @@
+package ru.job4j.loop;
+
+/**
+ * @author Toporov Pavel (mailto:per4mancerror@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Paint {
+    /**
+     * Строит пирамиду.
+     *
+     * @param height Количество строк.
+     * @return Пирамида.
+     */
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+}
+
---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.util.StringJoiner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PaintTest {
+
+    @Test
+    public void whenPyramid5() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(5);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("    ^    ")
+                                .add("   ^^^   ")
+                                .add("  ^^^^^  ")
+                                .add(" ^^^^^^^ ")
+                                .add("^^^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+}
