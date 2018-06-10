a535e36c0a9fb4ff711a782a854366acd7533806
Pavel Toporov <per4mancerror@gmail.com>
2018-06-10 13:39:52 +0300

    5.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,11 +1,42 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * @author Toporov Pavel (mailto:per4mancerror@gmail.com)
  * @version $Id$
  * @since 0.1
  */
 public class Paint {
+
+    /**
+     * Строит левую-пирамиду.
+     *
+     * @param height Количество строк.
+     * @return левая полу-пирамида.
+     */
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
+    /**
+     * Строит правую полу-пирамиду.
+     *
+     * @param height Количество строк.
+     * @return правая полу-пирамида.
+     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
     /**
      * Строит пирамиду.
      *
@@ -13,11 +44,18 @@ public class Paint {
      * @return Пирамида.
      */
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
