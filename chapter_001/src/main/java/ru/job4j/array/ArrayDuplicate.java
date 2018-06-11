a928a4635a8aadf0eaa281e2857423094e186e56
Pavel Toporov <per4mancerror@gmail.com>
2018-06-11 14:31:40 +0300

    6.8. Удаление дубликатов в массиве. [#225]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+
+import java.util.Arrays;
+/**
+ * @author Toporov Pavel (mailto:per4mancerror@gmail.com)
+ * @version $Id$
+ * @since 0.1*/
+public class ArrayDuplicate {
+    /**
+     * Удаляет дубликаты в массиве строк.
+     * @param array Массив.
+     * @return Массив без дубликатов.*/
+    public String[] remove(String[] array) {
+        int dub = array.length;
+        for (int i = 0; i < dub; i++) {
+            for (int j = i + 1; j < dub; j++) {
+                if (array[i].equals(array[j])) {
+                    array[j] = array[dub - 1];
+                    dub--;
+                    j--;
+                }
+            }
+        }
+        return Arrays.copyOf(array, dub);
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+public class ArrayDuplicateTest {
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        String[] input = {"Мама", "Мыла", "Раму", "Мама", "Раму", "Мыла", "Раму", "Мама"};
+        String[] expected = {"Мама", "Мыла", "Раму"};
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] result = duplicate.remove(input);
+        assertThat(result, arrayContainingInAnyOrder(expected));
+
+    }
+}
