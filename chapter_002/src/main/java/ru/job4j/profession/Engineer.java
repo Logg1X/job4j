c3ae1f53580754e8008428ff2d75ddb68a9b2731
Toporov Pavel <per4mancerror@gmail.com>
2018-06-18 12:53:11 +0300

    1. Реализация профессий в коде

---
+++ chapter_002/src/main/java/ru/job4j/profession/Engineer.java
@@ -0,0 +1,9 @@
+package ru.job4j.profession;
+
+public class Engineer extends Profession {
+
+    public void build(House house) {
+
+    }
+
+}
---
+++ chapter_002/src/main/java/ru/job4j/profession/Profession.java
@@ -0,0 +1,14 @@
+package ru.job4j.profession;
+
+public class Profession {
+    private String name;
+    private Profession profession;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
