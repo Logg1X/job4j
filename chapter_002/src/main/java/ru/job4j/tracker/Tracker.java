470a6da548f4d10d2458173e17388b70ead2c5e3
Pavel Toporov <per4mancerror@gmail.com>
2018-06-19 22:31:04 +0300

    2. Реализовать класс Tracker

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -3,12 +3,16 @@ package ru.job4j.tracker;
 import java.util.Arrays;
 import java.util.Date;
 
+/**
+ * @author Toporov Pavel (mailto:per4mancerror@gmail.com)
+ * @version $Id$
+ * @since 1.0
+ */
 public class Tracker {
     /**
      * Массив для хранение заявок.
      */
     private final Item[] items = new Item[100];
-
     /**
      * Указатель ячейки для новой заявки.
      */
@@ -33,15 +37,26 @@ public class Tracker {
      */
     private String generateId() {
         Date date = new Date();
-        long millis = date.getTime()%1000;
+        long millis = date.getTime() % 1000;
         int random = (int) (Math.random() * 100);
         return String.valueOf(millis + "#" + random);
     }
 
+    /**
+     * Метод заменяет Item в массиве items.
+     *
+     * @param id   номер заявки, который нужно заменить.
+     * @param item на эту заяку меняем.
+     */
     public void replace(String id, Item item) {
         this.items[getIndexAsInt(id)] = item;
     }
 
+    /**
+     * Метод узаляет заявку из массива.
+     *
+     * @param id номер заявки, которую нужно удалить.
+     */
     public void delete(String id) {
         int index = getIndexAsInt(id);
         int numMoved = this.items.length - index - 1;
@@ -49,16 +64,27 @@ public class Tracker {
         this.position--;
     }
 
+    /**
+     * Метод показывает все заявки.
+     *
+     * @return массив заявок.
+     */
     public Item[] findAll() {
-        return Arrays.copyOf(this.items, this.position);
+        return Arrays.copyOf(this.items, this.position + 1);
 
     }
 
+    /**
+     * Метод находит дубликаты имен заявок.
+     *
+     * @param key Имя заявки для поиска.
+     * @return массив дубликатов.
+     */
     public Item[] findByName(String key) {
         int counter = 0;
         Item[] result = new Item[this.items.length];
         for (Item item : this.items) {
-            if (item!=null && item.getName().equals(key)) {
+            if (item != null && item.getName().equals(key)) {
                 result[counter] = item;
                 counter++;
             }
@@ -66,16 +92,23 @@ public class Tracker {
         return Arrays.copyOf(result, counter);
     }
 
-
+    /**
+     * Метод находит заявку по ее номеру.
+     *
+     * @param id номер заявки
+     * @return заявка.
+     */
     public Item findById(String id) {
         int index = getIndexAsInt(id);
-        return index > 0 ? this.items[index] : null;
+        return index >= 0 ? this.items[index] : null;
     }
 
     private int getIndexAsInt(String id) {
         int index = -1;
         for (Item item : this.items) {
-            if (item.getId().equals(id)) {
+            if (item == null) {
+                break;
+            } else if (item.getId().equals(id)) {
                 index = Arrays.asList(this.items).indexOf(item);
             }
         }
--- chapter_002/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/test/java/ru/job4j/tracker/TrackerTest.java
@@ -2,7 +2,6 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,20 +10,20 @@ public class TrackerTest {
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
         Tracker tracker = new Tracker();
-        Item item = new Item("test1","testDescription",123L);
+        Item item = new Item("test1", "testDescription", 123L);
         tracker.add(item);
-        assertThat(tracker.findAll(), is(item));
+        assertThat(tracker.findAll()[0], is(item));
     }
 
 
     @Test
     public void whenReplaceNameThenReturnNewName() {
         Tracker tracker = new Tracker();
-        Item previous = new Item("test1","testDescription",123L);
+        Item previous = new Item("test1", "testDescription", 123L);
         // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
         tracker.add(previous);
         // Создаем новую заявку.
-        Item next = new Item("test2","testDescription2",1234L);
+        Item next = new Item("test2", "testDescription2", 1234L);
         // Проставляем старый id из previous, который был сгенерирован выше.
         next.setId(previous.getId());
         // Обновляем заявку в трекере.
@@ -32,5 +31,40 @@ public class TrackerTest {
         // Проверяем, что заявка с таким id имеет новые имя test2.
         assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
     }
+
+    @Test
+    public void whenDeleteItemWithIdThenReturnMassiveWithoutThisItem() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1", "1testDescription1", 1L);
+        Item item2 = new Item("test2", "2testDescription2", 2L);
+        Item item3 = new Item("test3", "3testDescription3", 3L);
+        Item item4 = new Item("test4", "4testDescription4", 4L);
+        Item item5 = new Item("test5", "5testDescription4", 5L);
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        tracker.delete(item2.getId());
+        tracker.delete(item3.getId());
+        assertThat(tracker.findAll()[1], is(item4));
+    }
+
+    @Test
+    public void whenInMassiveSeveralItemsWithTheSameNamesThenReturnThisDuplicates() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1", "1testDescription1", 1L);
+        Item item2 = new Item("test", "2testDescription2", 2L);
+        Item item3 = new Item("test3", "3testDescription3", 3L);
+        Item item4 = new Item("test", "4testDescription4", 4L);
+        Item item5 = new Item("test", "5testDescription4", 5L);
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        tracker.add(item5);
+        Item[] result = {item2, item4, item5};
+        assertThat(result, is(tracker.findByName("test")));
+    }
+
 
 }
