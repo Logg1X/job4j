a53d66726edc5db8f7364ebbf3ab04e6fa2b732c
Toporov Pavel <per4mancerror@gmail.com>
2018-07-16 18:56:38 +0300

    1. Организовать сортировку User

--- chapter_002/src/main/java/ru/job4j/collections/models/User.java
+++ chapter_002/src/main/java/ru/job4j/collections/models/User.java
@@ -1,14 +1,25 @@
 package ru.job4j.collections.models;
 
-public class User {
+public class User implements Comparable<User> {
     private int id;
+    private int age;
     private String name;
     private String city;
+
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
 
     public User(int id, String name, String city) {
         this.id = id;
         this.name = name;
         this.city = city;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     public int getId() {
@@ -21,5 +32,18 @@ public class User {
 
     public String getCity() {
         return city;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        return Integer.compare(this.age, o.age);
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + this.id + System.lineSeparator() +
+                "Age: " + this.age + System.lineSeparator() +
+                "Name: " + this.name + System.lineSeparator() +
+                "City: " + this.city + System.lineSeparator();
     }
 }
