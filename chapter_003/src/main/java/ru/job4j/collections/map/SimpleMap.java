1c6d465aea85da933fc123be75a485646fabf29f
Pavel Toporov <per4mancerror@gmail.com>
2018-08-23 00:00:16 +0300

    8. Реализовать собственную структуру данных - HashMap

---
+++ chapter_003/src/main/java/ru/job4j/collections/map/SimpleMap.java
@@ -0,0 +1,114 @@
+package ru.job4j.collections.map;
+
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleMap<K, V> implements Iterable<SimpleMap.Node> {
+    private Node[] map;
+    private int modCount;
+    private int size;
+
+    public SimpleMap(int size) {
+        this.map = new Node[size];
+        this.modCount = 0;
+        this.size = size;
+    }
+
+    public boolean insert(K key, V value) {
+        boolean result = false;
+        int position = getPosition(key);
+        if (position >= size) {
+            size += position;
+            Node[] newArray = new Node[size];
+            System.arraycopy(map, 0, newArray, 0, map.length);
+            map = newArray;
+        }
+        if (map[position] == null) {
+            map[position] = new Node<>(key, value);
+            this.modCount++;
+            result = true;
+        }
+        return result;
+    }
+
+    public V get(K key) {
+        Node<K, V> node = this.map[getPosition(key)];
+        V result = null;
+        if (node != null && node.key.hashCode() == key.hashCode() && node.key.equals(key)) {
+            result = node.value;
+        }
+        return result;
+    }
+
+    public boolean delete(K key) {
+        boolean result = false;
+        int position = getPosition(key);
+        if (map[position] != null
+                && map[position].key.hashCode() == key.hashCode()
+                && map[position].key.equals(key)) {
+            map[position] = null;
+            result = true;
+            modCount++;
+        }
+        return result;
+    }
+
+    private int getPosition(K key) {
+        return Math.abs((31 * key.hashCode() % 10)) * 2 + 5;
+    }
+
+    @Override
+    public Iterator<Node> iterator() {
+        return new Iterator<Node>() {
+            int expectedModCount = modCount;
+            int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                boolean result = false;
+                while (this.cursor < map.length) {
+                    if (map[cursor] == null) {
+                        cursor++;
+                        continue;
+                    }
+                    if (map[cursor].getClass().equals(Node.class)) {
+                        result = true;
+                        break;
+                    }
+                }
+                return result;
+            }
+
+            @Override
+            public Node next() {
+                Node result;
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException("NoSuchElementException");
+                }
+                result = map[this.cursor++];
+
+                return result;
+            }
+        };
+    }
+
+    static class Node<K, V> {
+        final K key;
+        V value;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return key + " " + value;
+        }
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/collections/map/SimpleMapTest.java
@@ -0,0 +1,149 @@
+package ru.job4j.collections.map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleMapTest {
+
+    private SimpleMap<User, String> map;
+    private User pasha;
+    private User sasha;
+    private User yulya;
+
+
+    private class User {
+        private String firstName;
+        private int age;
+
+        public User(String firstName, int age) {
+            this.firstName = firstName;
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+
+                return false;
+            }
+            User user = (User) o;
+            return age == user.age
+                    && Objects.equals(firstName, user.firstName);
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(firstName, age);
+        }
+
+        @Override
+        public String toString() {
+            return firstName
+                    + " "
+                    + age
+                    + " лет.";
+        }
+    }
+
+
+    @Before
+    public void setUp() throws Exception {
+        pasha = new User("Pasha", 26);
+        sasha = new User("Sasha", 27);
+        yulya = new User("Yula", 24);
+
+        map = new SimpleMap<>(5);
+        map.insert(pasha, "Qiwi");
+        map.insert(sasha, "Qiwi");
+        map.insert(yulya, "Bosh");
+    }
+
+    @Test
+    public void whenAddSomeOneKeyAndValue() {
+        User somUser2 = new User("SimeUser2", 66666);
+        assertThat(map.insert(somUser2, "sdfsf"), is(true));
+    }
+
+    @Test
+    public void whenCollizionThenFalse() {
+        User somUser1 = new User("SimeUser1", 9999);
+        assertFalse(map.insert(somUser1, "SomeWork"));
+    }
+
+    @Test
+    public void whenDuplicateKey() {
+        User pasha23 = new User("Pasha", 26);
+        assertFalse(map.insert(pasha23, "Epam"));
+    }
+
+    @Test
+    public void whenGetSomOneElement() {
+        assertThat(map.get(pasha), is("Qiwi"));
+    }
+
+    @Test
+    public void whenNoElementToMap() {
+        User someUser1 = new User("SimeUser1", 9999);
+        assertNull(map.get(someUser1));
+    }
+
+    @Test
+    public void whenGetValue() {
+        assertThat(map.get(pasha), is("Qiwi"));
+    }
+
+    @Test
+    public void whenDeleteSomOneElement() {
+        assertThat(map.get(pasha), is("Qiwi"));
+        assertTrue(map.delete(pasha));
+        assertNull(map.get(pasha));
+    }
+
+    @Test
+    public void whenDeleteNoExistElement() {
+        User somUser1 = new User("SimeUser1", 9999);
+        assertFalse(map.delete(somUser1));
+    }
+
+    @Test
+    public void whenIterateMap() {
+        Iterator iterator = map.iterator();
+        assertTrue(iterator.hasNext());
+        assertThat(iterator.next().toString(), is("Yula 24 лет. Bosh"));
+        assertTrue(iterator.hasNext());
+        assertThat(iterator.next().toString(), is("Sasha 27 лет. Qiwi"));
+        assertTrue(iterator.hasNext());
+        assertThat(iterator.next().toString(), is("Pasha 26 лет. Qiwi"));
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenIterateWithNoSuchElementException() {
+        Iterator iterator = map.iterator();
+        assertThat(iterator.next().toString(), is("Yula 24 лет. Bosh"));
+        assertThat(iterator.next().toString(), is("Sasha 27 лет. Qiwi"));
+        assertThat(iterator.next().toString(), is("Pasha 26 лет. Qiwi"));
+        iterator.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenIterateWithConcurrentModificationException() {
+        Iterator iterator = map.iterator();
+        iterator.next();
+        assertTrue(map.delete(pasha));
+        iterator.next();
+    }
+}
